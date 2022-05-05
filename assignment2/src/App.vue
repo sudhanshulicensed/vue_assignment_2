7c3b45c9d2feb85d03cbe89926013ae20efcc117
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2022-05-05 10:02:47 +0530

    Reuploaded Structure and Pushed again with Own extra tasks

---
+++ assignment2/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <BaseButton @isClicked='test("Hey Hey Hey")' :staySameText="primary"></BaseButton>
+    <BaseButton @isClicked='reverse' :isReverse="reverseText"></BaseButton>
+  </div>
+</template>
+
+<script>
+import BaseButton from "./components/button.vue"
+
+
+export default {
+  
+  name: 'App',
+  components: {
+    BaseButton,
+  },
+  data() {
+    return {
+      reverseText: "Primary" ,
+      primary: "Primary"
+    }
+  },
+
+  methods: {
+    test(string) {
+      alert(string);
+    },
+    reverse(title) {
+      console.log(title);
+            this.reverseText = title.split("").reverse().join("");
+            console.log(this.reverseText);
+        }
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 70px;
+  height: 400px;
+  background-color: rgba(184, 233, 148, 0.6);
+  border-top: 5px solid rgba(68, 189, 50,1.0);
+}
+</style>
---
+++ assignment2/src/components/button.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="button">
+        <p @click="happened" class="btn-text">{{ isReverse }} {{ staySameText }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['isReverse', "staySameText"],
+    name: "BaseButton",
+    data() {
+        return {
+            title: "Primary",
+        }
+    },
+    methods: {
+        happened () {
+            this.$emit('isClicked', this.isReverse);
+            console.log(this.title, "Click Happened");
+        },
+    }
+}
+</script>
+
+<style scoped>
+    .button{
+        display: flex;
+        flex-direction: column;
+        align-content: center;
+        justify-content: center;
+        text-align: center;
+        box-sizing: border-box;
+        width: 300px;
+        height: 100px;
+        background-color: rgba(126, 214, 223, 0.6);
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 20px;
+        border-radius: 10px;
+    }
+
+    .btn-text{
+        font-family: sans-serif;
+        font-size: 2em;
+        color: #ffffff;
+        font-weight: 400;
+    }
+
+
+</style>
